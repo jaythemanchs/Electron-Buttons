18749069bcf3332013919ee12724e2dc61a19ae3
BuildTools <unconfigured@null.spigotmc.org>
2022-09-25 18:32:34 +1000

    Updates

---
+++ color.js
@@ -0,0 +1,53 @@
+function hexToHSL(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    r = parseInt(result[1], 16);
+    g = parseInt(result[2], 16);
+    b = parseInt(result[3], 16);
+    r /= 255, g /= 255, b /= 255;
+    var max = Math.max(r, g, b), min = Math.min(r, g, b);
+    var h, s, l = (max + min) / 2;
+    if (max == min) {
+        h = s = 0; // achromatic
+    } else {
+        var d = max - min;
+        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+        switch (max) {
+            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+            case g: h = (b - r) / d + 2; break;
+            case b: h = (r - g) / d + 4; break;
+        }
+        h /= 6;
+    }
+    var HSL = new Object();
+    HSL['h'] = h * 360;
+    HSL['s'] = s * 100;
+    HSL['l'] = l * 100;
+    return HSL;
+}
+
+function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+
+function rgbToHex(r, g, b) {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+function color(color) {
+    electronAPI.setTitle({
+        color: `rgb(${color}, ${color}, ${color})`
+    })
+    return hexToHSL(rgbToHex(Number(color), Number(color), Number(color)))
+}
+
+function HSLToHex(h, s, l) {
+    l /= 100;
+    const a = s * Math.min(l, 1 - l) / 100;
+    const f = n => {
+        const k = (n + h / 30) % 12;
+        const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+        return Math.round(255 * color).toString(16).padStart(2, '0');   // convert to Hex and prefix "0" if needed
+    };
+    return `#${f(0)}${f(8)}${f(4)}`;
+}
--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 let buttonCount = 0
 let buttons = []
+const templateOptions = ['id', 'height', 'icon', ]
 
 class TitleBarButton {
     constructor(browserWindow, options) {
         if (browserWindow != null && browserWindow != undefined) {
             if (browserWindow['webContents'] != undefined) {
-                if (typeof options == 'object' && options != null)
-                buttonCount++
-                this.id = buttonCount
+                if (typeof options == 'object' && options != null) {
+                    if (Object.keys(options) == templateOptions) {
+                        this.id = options.id
+                    }
+                }
             } else {
                 throw new Error('BrowserWindow must be of type \'BrowserWindow\'')
             }
@@ -15,14 +18,15 @@ class TitleBarButton {
             throw new Error('BrowserWindow must be of type \'BrowserWindow\'')
         }
     }
-    id = 0
-    static getPropertiesOfWindow = () => {
+    id
+    height
+    static getPropertiesOfWindow = (browserWindow) => {
 
     }
     static pixelsConsumed = (browserWindow) => {
         return {
             width: (buttonCount * 46),
-            height: 0
+            height: this.getPropertiesOfWindow(browserWindow)
         }
     }
 }
