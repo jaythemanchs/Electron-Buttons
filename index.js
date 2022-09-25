c7c89f234e15bdc83085d3268973b9ab8c25a0d8
BuildTools <unconfigured@null.spigotmc.org>
2022-09-25 12:31:54 +1000

    Updates

---
+++ index.js
@@ -0,0 +1,27 @@
+let buttonCount = 0
+import { BrowserWindow } from "electron";
+
+class TitleBarButton {
+    constructor(browserWindow, options) {
+        if (browserWindow != null && browserWindow != undefined) {
+            if (browserWindow['webContents'] != undefined) {
+                if (typeof options == 'object' && options != null)
+                buttonCount++
+                this.id = buttonCount
+            } else {
+                throw new Error('BrowserWindow must be of type \'BrowserWindow\'')
+            }
+        } else {
+            throw new Error('BrowserWindow must be of type \'BrowserWindow\'')
+        }
+    }
+    id = 0
+    static pixelsConsumed = (browserWindow) => {
+        return {
+            width: (buttonCount * 46),
+            height: 0
+        }
+    }
+}
+
+new TitleBarButton(null)
